c0003f403673d2e0ce60cb5db4395411eda558c5
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-09 22:08:56 +0200

    done

---
+++ Task001/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число ");
+int num = int.Parse(Console.ReadLine());
+String str = num.ToString();
+if (str.Length != 3){Console.WriteLine("Вы ввели не трёхзначное число ");} 
+else{Console.WriteLine("Вторая цифра введенного Вами числа " + str[1]);}
